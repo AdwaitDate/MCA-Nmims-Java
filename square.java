93e3b6f18104780182ee60fd1fdc1923302e1dae
AdwaitDate <119007598+AdwaitDate@users.noreply.github.com>
2024-07-16 15:48:24 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class square {
+    public static void main(String[] args) {
+        int n1,n2;
+        Scanner sc= new Scanner(System.in);
+        
+        System.out.println("Enter no .1.");
+        n1=sc.nextInt();
+        System.out.println("Enter no .2");
+        n2=sc.nextInt();
+        int sum=n1+n2;
+        System.out.println("The Sum of 2 Numbers is"+sum);
+        // sc.close();
+    }
+}
